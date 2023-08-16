a3dfd9affe4398d48507b6d35ac41131a4914a7a
unknown <karanarora654321@gmail.com>
2023-08-16 14:33:54 +0530

    first commit

---
+++ pms/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from pms.models import UserProfile, Blogs, Property, PropertyImages, \
+    PropertyMembership, PropertyContacts, Contact
+
+admin.site.register(UserProfile)
+admin.site.register(Blogs)
+admin.site.register(Property)
+admin.site.register(PropertyImages)
+admin.site.register(PropertyMembership)
+admin.site.register(PropertyContacts)
+admin.site.register(Contact)
